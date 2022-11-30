07925374c7c6eb318fdf9d053f2e67e5ca84ce6d
Parth <68063528+Parthparthu@users.noreply.github.com>
2022-11-30 14:19:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
+# do not change the code until mentioned
 import pyautogui
 import time
 while True:
 	time.sleep(0.5)
-	pyautogui.typewrite('SPAM')
+	pyautogui.typewrite('SPAM')# enter your message over here
 	pyautogui.press('enter')
